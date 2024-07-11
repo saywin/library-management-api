51ea091fcd9c059af989f1aac9ad68672a0877a6
tiway <stilon670@gmail.com>
2024-07-11 14:08:55 +0300

    Added class Book to models.py

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        PENDING = "PENDING"
+        PAID = "PAID"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=50, choices=CoverChoices.choices)
+    inventory = models.IntegerField(validators=MinValueValidator(0))
+    daily_fee = models.DecimalField(max_digits=8, decimal_places=2)
