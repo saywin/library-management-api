010256f3b796947a58d06ddd3cb56e98f4d671e8
tiway <stilon670@gmail.com>
2024-07-12 12:14:45 +0300

    fix book coverchoices and add to fields "id" in BookSerializer

--- books/models.py
+++ books/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class Book(models.Model):
     class CoverChoices(models.TextChoices):
-        PENDING = "PENDING"
-        PAID = "PAID"
+        HARD = "HARD"
+        SOFT = "SOFT"
 
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     cover = models.CharField(max_length=50, choices=CoverChoices.choices)
-    inventory = models.IntegerField(validators=[MinValueValidator(0)])
+    inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=8, decimal_places=2)
