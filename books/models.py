c98f39b235169489169586db6a7aab6181a16c8a
tiway <stilon670@gmail.com>
2024-07-13 11:07:42 +0300

    Added method str to Book model

--- books/models.py
+++ books/models.py
@@ -12,3 +12,6 @@ class Book(models.Model):
     cover = models.CharField(max_length=50, choices=CoverChoices.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"Book: {self.title}, author : {self.author} "
