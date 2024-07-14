f81c8e4d7bfe3c57f06a184b3796f32b432dae59
tiway <stilon670@gmail.com>
2024-07-14 16:06:03 +0300

    Create Payments Model

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from borrowings.models import Borrowing
+
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "PENDING"
+        PAID = "PAID"
+
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "PAYMENT"
+        FINE = "FINE"
+
+    status = models.CharField(max_length=10, choices=StatusChoices.choices)
+    type = models.CharField(max_length=10, choices=StatusChoices.choices)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+    session_url = models.URLField(null=True, blank=True)
+    session_id = models.CharField(max_length=100, blank=True, null=True)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.get_type_display()} - {self.status}"
+
+    def get_absolute_url(self):
+        return reverse("payment-detail", args=[str(self.id)])
