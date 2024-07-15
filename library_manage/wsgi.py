0291d7352e29ca5b310598cf0253edd073d1d626
tiway <stilon670@gmail.com>
2024-07-15 14:02:23 +0300

    Added views payments

--- library_manage/wsgi.py
+++ library_manage/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for liberty_manage project.
+WSGI config for library_manage project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'liberty_manage.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_manage.settings")
 
 application = get_wsgi_application()
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.http import HttpResponse
+from dotenv import load_dotenv
+
+import stripe
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from payments.models import Payment
+from payments.permissions import IsAdminOrOwnerUser
+from payments.serializers import PaymentSerializer
+
+load_dotenv()
+
+
+class PaymentListAPIView(generics.ListCreateAPIView):
+    serializer_class = PaymentSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Payment.objects.all()
+        else:
+            return Payment.objects.filter(user=user)
+
+
+class PaymentDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = (IsAdminOrOwnerUser,)
+
+
+stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+
+
+class CreateCheckoutSessionView(APIView):
+    def post(self, request, *args, **kwargs):
+        payment_id = request.data.get("payment_id")
+        payment = get_object_or_404(Payment, id=payment_id)
+
+        session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            line_items=[
+                {
+                    "price_data": {
+                        "currency": "usd",
+                        "product_data": {
+                            "name": "Library Payment",
+                        },
+                        "unit_amount": int(payment.money_to_pay * 100),
+                    },
+                    "quantity": 1,
+                }
+            ],
+            mode="payment",
+            success_url="http://localhost:8000/payments/success/",
+            cancel_url="http://localhost:8000/payments/cancel/",
+        )
+
+        payment.session_id = session.id
+        payment.session_url = session.url
+        payment.save()
+
+        return Response({"id": session.id}, status=status.HTTP_200_OK)
+
+
+class PaymentSuccessView(APIView):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Payment was successful.")
+
+
+class PaymentCancelView(APIView):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Payment was cancelled.")
