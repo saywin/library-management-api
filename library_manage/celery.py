766ba15541cf2cb72669742bdd2ac3876321d19f
tiway <stilon670@gmail.com>
2024-07-15 13:53:35 +0300

    Added PaymentSerializer

--- library_manage/celery.py
+++ library_manage/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liberty_manage.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_manage.settings")
 
-app = Celery("liberty_manage")
+app = Celery("library_manage")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
