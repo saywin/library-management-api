daea3c720a1561712f33bc647093b16b0f15c181
tiway <stilon670@gmail.com>
2024-07-18 16:54:16 +0300

    Solution

--- library_manage/settings.py
+++ library_manage/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0wo#4@bi3&^2c_o7rjpt#_wf3&cp-^i@b%3tyq4md#k@)cmpem"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
